475f975b60289f34dec85fe2a241b4c5ba3937d5
Valera Temshik <skywize39@gmail.com>
2026-02-23 22:17:44 +0300

    Add files via upload

---
+++ Translate.py
@@ -0,0 +1,22 @@
+import time
+
+from deep_translator import GoogleTranslator
+
+while True:
+    print("1 -> Translate")
+    print("2 -> Exit")
+    choose_action = (input("Enter your action >> ")).strip()
+    if choose_action == "1":
+        choose_language = input(
+            "Enter target language -- 1.ru 2.en 3.de 4.fr 5.es 6.ja 7.it >> "
+        )
+        translat = GoogleTranslator(source="auto", target=choose_language)
+        text = input("Enter your text >> ")
+        print(f"Your translated text -> {translat.translate(text)}")
+        input("Press any key...")
+    elif choose_action == "2":
+        print("Your action is Exit, Bye!")
+        time.sleep(2)
+        break
+    else:
+        print("Your right action!")
